4167068e3b950b01f56db926c44acd03bf5637ac
caixianghui <caixianghui@zhangjingguodeiMac.local>
2016-05-05 09:04:39 +0800

    add button

--- 8dibBusiness/8dibBusiness/ViewController.m
+++ 8dibBusiness/8dibBusiness/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.view.backgroundColor = nil;
     ///dfgrgrg
+    UIButton *button = nil;
 }
 
 - (void)didReceiveMemoryWarning {
